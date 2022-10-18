0d7921262e64802524f92f18121d733b965bbf09
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-19 00:13:27 +0530

    Flip Coin and print percentage of Heads and Tails

---
+++ Day5Assignment/Day5Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Day5Assignment;
+
+namespace Day5Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day5Problems num = new Day5Problems();
+            num.flipCoin();
+        }
+    }
+}
