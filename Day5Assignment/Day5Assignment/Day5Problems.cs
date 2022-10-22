4dcc1a56f85915bf43ef521815bcf7b69aa3aa46
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-22 07:52:29 +0530

    Check Whether an Alphabet is Vowel or Consonant

--- Day5Assignment/Day5Assignment/Day5Problems.cs
+++ Day5Assignment/Day5Assignment/Day5Problems.cs
@@ -144,5 +144,26 @@ namespace Day5Assignment
             }
 
         }
+
+        public void Check_Char_Vowel_Or_Consonant ()
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            // Condition for vowel checking
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel.");
+
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+        }
     }
 }
--- Day5Assignment/Day5Assignment/Program.cs
+++ Day5Assignment/Day5Assignment/Program.cs
@@ -15,6 +15,7 @@ namespace Day5Assignment
             num.Factor();
             num.Swap_Two_Number();
             num.Check_Even_Or_Odd();
+            num.Check_Char_Vowel_Or_Consonant();
         }
     }
 }
